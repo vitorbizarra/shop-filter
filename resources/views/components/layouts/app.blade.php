b6d259d0dc863bf85d88c5a43e99f1956ae80e8d
vitorbizarra <eu@vitorbizarra.dev.br>
2025-10-20 23:11:47 -0300

    wip: Home full-page component

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,5 +1,12 @@
-<x-layouts.app.sidebar :title="$title ?? null">
-    <flux:main>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ 
+        <title>{{ $title ?? 'Page Title' }}</title>
+    </head>
+    <body>
         {{ $slot }}
-    </flux:main>
-</x-layouts.app.sidebar>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Livewire;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+Route::get('/', Livewire\Home::class)->name('home');
